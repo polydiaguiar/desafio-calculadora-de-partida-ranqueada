3bc8a89c2561cf709fe9662ca02944f6c4115b82
unknown <polyana.karynne@hotmail.com>
2024-01-13 09:51:07 -0300

    test: cria variáveis e funções

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-//  Calculadora de partidas Rankeadas: O Que deve ser utilizado
-//- Variáveis
-//- Operadores
-//- Laços de repetição
-//- Estruturas de decisões
-//- Funções
-
-//## Objetivo: Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
-//depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
 
 //Se vitórias for menor do que 10 = Ferro
 //Se vitórias for entre 11 e 20 = Bronze
@@ -16,17 +7,51 @@
 //Se vitórias for entre 91 e 100= Lendário
 //Se vitórias for maior ou igual a 101 = Imortal
 
-//## Saída: Ao final deve se exibir uma mensagem:
-//"O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"
 
-let nomeDoHeoroi = "Alberto"
-let saldoDeVitorias = diferença(5,3)
+
+
+
+let nomeDoHeroi = "Alberto"
+let quantidadeDeVitorias = 10
+let quantidadeDeDerrotas = 3
+let saldoDeVitorias = diferença(quantidadeDeVitorias,quantidadeDeDerrotas)
+let nivelDoHeroi = nivel(quantidadeDeVitorias)
+console.log("\nO  herói " + nomeDoHeroi + " tem saldo de " + saldoDeVitorias + " vitórias. Seu nível é \n" +nivelDoHeroi)
+
+
+ nomeDoHeroi = "José"
+ quantidadeDeVitorias = 5
+ quantidadeDeDerrotas = 3
+ saldoDeVitorias = diferença(quantidadeDeVitorias,quantidadeDeDerrotas)
+ nivelDoHeroi = nivel(quantidadeDeVitorias)
+console.log("\nO herói " + nomeDoHeroi + " tem saldo de " + saldoDeVitorias + " vitórias. Seu nível é \n" + nivelDoHeroi)
 
 
 function diferença (quantidadeDeVitorias, quantidadeDeDerrotas){
 
 return quantidadeDeVitorias-quantidadeDeDerrotas
 
+}
+
+function nivel (quantidadeDeVitorias){
+
+    if (quantidadeDeVitorias<=10){
+        nivelDoHeroi == "Ferro"
+
+    } else if ((10<quantidadeDeVitorias) && (quantidadeDeVitorias<20)){
+        nivelDoHeroi == "Bronze"
+
+    }
 
 
-} console.log("O " + nomeDoHeoroi + " tem saldo de " + saldoDeVitorias + " vitórias. Seu nível é " )
+
+
+
+
+
+
+
+}
+
+
+
